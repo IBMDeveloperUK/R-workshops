53bf16dc4b6bedf5ab95408f1097c5776e9bac5e
Ross Cruickshank <ross.cruickshank@uk.ibm.com>
2020-07-21 11:02:45 +0100

    fix for tidyr version mismatch

---
+++ notebooks/machine-learning/install-backlevel-tidr.R
@@ -0,0 +1,7 @@
+
+#compatible with already-installed tidyselect
+#not too old - announced September 2019
+
+link <- "https://cran.r-project.org/src/contrib/Archive/tidyr/tidyr_1.0.0.tar.gz"
+install.packages(link,repos=NULL)
+library(tidyr)
